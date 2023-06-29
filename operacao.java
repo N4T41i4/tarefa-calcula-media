cb0c8ca7b29fa2a2ed043a3ac15faf0976654544
Natalia Silva <natalia@exemplo.com>
2023-06-28 23:48:56 -0300

    calcular as medias

---
+++ operacao.java
@@ -0,0 +1,15 @@
+public class operacao {
+    public static void main(String args[]){
+
+        calculanota();
+    }
+    public static void calculanota(){
+        System.out.println("++++++++++Calcula Média++++++++++");
+        int nota1=15;
+        int nota2=18;
+        int nota3=50;
+        int nota4=10;
+        int media=(nota1+nota2+nota3+nota4)/4;
+        System.out.println("A média da notas é:"+ media);
+    }
+}
